f3fc1b67d94c6a73e3614682c2b1153ea3f823de
krittin <krittin.tt@gmail.com>
2023-02-11 20:54:23 +0000

    fixed error splitting network file

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,3 @@
-
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
@@ -19,48 +14,3 @@ resource "aws_internet_gateway" "igw" {
   }
 
 }
-
-resource "aws_subnet" "private_subnets" {
-  count = length(var.private_subnet_cidrs)
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = element(var.private_subnet_cidrs, count.index)
-  availability_zone = element(data.aws_availability_zones.available.names, count.index)
- 
-  tags = {
-   name = "eks private subnet ${count.index+1}"
-  }
-}
-
-
-resource "aws_subnet" "public_subnets" {
-  count = length(var.public_subnet_cidrs)
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = element(var.public_subnet_cidrs, count.index)
-  availability_zone = element(data.aws_availability_zones.available.names, count.index)
- 
-  tags = {
-   name = "eks public subnet ${count.index+1}"
-  }
-}
-
-resource "aws_eip" "nat_ips" {
-  count = length(var.private_subnet_cidrs)
-
-  tags = {
-    name = "eip for nat ${count.index+1}"
-  }
-}
-
-resource "aws_nat_gateway" "nat_gateways" {
-  count = length(var.private_subnet_cidrs)
-  allocation_id = element(aws_eip.nat_ips[*].id, count.index)
-  subnet_id = element(aws_subnet.public_subnets[*].id, count.index)
-
-  tags = {
-    name = "nat ${count.index+1}"
-  }
-
-  depends_on = [ aws_internet_gateway.igw ]
-
-}
-
