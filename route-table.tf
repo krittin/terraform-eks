3ae77b7c58d13e5c5d67cf0945a746fc2eb853be
krittin <krittin.tt@gmail.com>
2023-02-11 21:48:57 +0000

    added routing table for public subnets

--- route-table.tf
+++ route-table.tf
@@ -18,3 +18,24 @@ resource "aws_route_table_association" private_rt_associations {
   subnet_id = element(aws_subnet.private_subnets[*].id, count.index)
   route_table_id = element(aws_route_table.private_route_tables[*].id, count.index)
 }
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    name = "routing table for public subnets"
+  }
+
+}
+
+resource "aws_route_table_association" public_rt_associations {
+  count = length(var.public_subnet_cidrs)
+
+  subnet_id = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.public_route_table.id
+}
