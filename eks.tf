2a23fa0d383dff1afeec304a7a9d8acba886bbaf
krittin <krittin.tt@gmail.com>
2023-02-12 13:50:07 +0000

    added eks cluster

---
+++ eks.tf
@@ -0,0 +1,16 @@
+resource "aws_eks_cluster" "eks_cluster" {
+  name = var.eks_cluster_name
+  version = var.k8s_version
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.private_subnets[*].id
+  }
+
+  kubernetes_network_config {
+    service_ipv4_cidr = var.pod_cidr
+  }
+
+  depends_on = [ aws_iam_role_policy_attachment.eks_cluster_role_with_cluster_policy ]
+
+}
