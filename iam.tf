ef587b7d64cb41bacd47d94525267947ef3c5658
krittin <krittin.tt@gmail.com>
2023-02-12 22:11:47 +0000

    removed commented code

--- iam.tf
+++ iam.tf
@@ -44,21 +44,6 @@ resource "aws_iam_role" "eks_node_group_role" {
   })
 }
 
-#resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEKSWorkerNodePolicy"{
-#  role = aws_iam_role.eks_node_group_role.name
-#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-#}
-
-#resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEKS_CNI_Policy"{
-#  role = aws_iam_role.eks_node_group_role.name
-#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-#}
-
-#resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEC2ContainerRegistryReadOnly"{
-#  role = aws_iam_role.eks_node_group_role.name
-#  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-#}
-
 resource "aws_iam_role_policy_attachment" "eks_node_group_role_with_policies" {
 
   for_each = toset([
