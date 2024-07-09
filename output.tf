aa9011492a061c53729c817c07c37df83cfbd15f
krittin <krittin.tt@gmail.com>
2024-07-09 20:28:58 +0100

    added output for command to config kubectl

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "endpoint" {
 output "kubeconfig_certificate_authority_data" {
   value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
 }
+
+output "config_kubectl" {
+  value = "aws eks update-kubeconfig --region ${var.aws_region} --name ${var.eks_cluster_name}"
+}
