52979cddb3bb2287d4f6ba6170216d88e1dbd336
krittin <krittin.tt@gmail.com>
2024-07-10 21:35:34 +0100

    removed k8s cert data from output

--- output.tf
+++ output.tf
@@ -2,10 +2,6 @@ output "endpoint" {
   value = aws_eks_cluster.eks_cluster.endpoint
 }
 
-output "kubeconfig_certificate_authority_data" {
-  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
-}
-
 output "config_kubectl" {
   value = "aws eks update-kubeconfig --region ${var.aws_region} --name ${var.eks_cluster_name}"
 }
