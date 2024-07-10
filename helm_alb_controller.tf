dfb58b12e732cd33719d4bf601f85f1cb1fd1a3e
krittin <krittin.tt@gmail.com>
2024-07-10 21:34:57 +0100

    added helm resource for install aws lb controller

---
+++ helm_alb_controller.tf
@@ -0,0 +1,28 @@
+provider "helm" {
+  kubernetes {
+		host = aws_eks_cluster.eks_cluster.endpoint
+		cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+		token = data.aws_eks_cluster_auth.eks.token
+	}
+}
+
+resource "helm_release" "alb_controller" {
+	namespace = "kube-system"
+	name = "aws-load-balancer-controller"
+	repository = "https://aws.github.io/eks-charts"
+	chart = "aws-load-balancer-controller"
+	version = var.alb_controller_helmchart_version
+
+	set {
+	  name = "clusterName"
+	  value = aws_eks_cluster.eks_cluster.name
+	}
+	set {
+	  name = "serviceAccount.create"
+	  value = "false"
+	}
+	set {
+	  name = "serviceAccount.name"
+	  value = kubernetes_service_account.alb_controller.metadata[0].name
+	}
+}
